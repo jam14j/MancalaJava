5aa868962d6e17d4bfc6290ed0c79d25c31596f3
Juan Martinez <jam14j@my.fsu.edu>
2019-07-25 22:41:46 -0400

    Changed font

--- src/sample/game_Controller.java
+++ src/sample/game_Controller.java
@@ -4,8 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class game_Controller {
-    @FXML
-    private Label player_settings;
+    @FXML private Label player_settings;
     public void setSettings(){
         player_settings.setText("Versus");
     }
