9a87e4ff7fc7b1c7bd7cc41b7eff00ac5a1bc3ee
Juan Martinez <jam14j@my.fsu.edu>
2019-07-25 21:56:35 -0400

    I got the buttons to work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+	@FXML public void startSingle(ActionEvent event) {
+		System.out.println("Hello Single!");
+	}
+
+	@FXML public void startVersus(ActionEvent event) {
+		System.out.println("Hello Versus!");
+	}
 
 }
