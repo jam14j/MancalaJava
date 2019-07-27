d4819e78cfc1ab5a9468f244d49663462c79d46d
grant0500 <47281743+grant0500@users.noreply.github.com>
2019-07-26 23:19:31 -0400

    p1 works but p2 doesnt for mancala goal

--- src/sample/game_Controller.java
+++ src/sample/game_Controller.java
@@ -8,83 +8,141 @@ import javafx.scene.control.Label;
 import javafx.scene.shape.Ellipse;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class game_Controller {
-    @FXML private Label player_settings,turn_label, p1_mancala_label, p2_mancala_label;
+    @FXML private Label player_settings, turn_label, p1_mancala_label, p2_mancala_label;
     @FXML private Ellipse p1_mancala, p2_mancala;
     @FXML private List<Button> pile_list_button;
     @FXML private List<Ellipse> pile_list_ellipse;
     Boolean whosTurn = true;
 
-    public void Initialize(){
-        for (int i =0; i <12; i++){
+    public void Initialize() {
+        for (int i = 0; i < 12; i++) {
             Button button = pile_list_button.get(i);
             button.setStyle("-fx-background-color: transparent;");
-//            final int whichButton = i;
+            takeTurn();
             button.setOnAction(new EventHandler<ActionEvent>() {
-                @Override public void handle(ActionEvent e) {
+                @Override
+                public void handle(ActionEvent e) {
                     int whichButton = -1;
-                    switch(button.getId()) {
-                        case "pile_button1": whichButton = 0; break;
-                        case "pile_button2": whichButton = 1; break;
-                        case "pile_button3": whichButton = 2; break;
-                        case "pile_button4": whichButton = 3; break;
-                        case "pile_button5": whichButton = 4; break;
-                        case "pile_button6": whichButton = 5; break;
-                        case "pile_button7": whichButton = 6; break;
-                        case "pile_button8": whichButton = 7; break;
-                        case "pile_button9": whichButton = 8; break;
-                        case "pile_button10": whichButton = 9; break;
-                        case "pile_button11": whichButton = 10; break;
-                        case "pile_button12": whichButton = 11; break;
+                    switch (button.getId()) {
+                        case "pile_button1":
+                            whichButton = 0;
+                            break;
+                        case "pile_button2":
+                            whichButton = 1;
+                            break;
+                        case "pile_button3":
+                            whichButton = 2;
+                            break;
+                        case "pile_button4":
+                            whichButton = 3;
+                            break;
+                        case "pile_button5":
+                            whichButton = 4;
+                            break;
+                        case "pile_button6":
+                            whichButton = 5;
+                            break;
+                        case "pile_button7":
+                            whichButton = 6;
+                            break;
+                        case "pile_button8":
+                            whichButton = 7;
+                            break;
+                        case "pile_button9":
+                            whichButton = 8;
+                            break;
+                        case "pile_button10":
+                            whichButton = 9;
+                            break;
+                        case "pile_button11":
+                            whichButton = 10;
+                            break;
+                        case "pile_button12":
+                            whichButton = 11;
+                            break;
                     }
-                    takeTurn();
                     int amt = Integer.parseInt(button.getText());
-                    System.out.println(whichButton);
-//                    int nextButton = whichButton+1;
-                    for (int k =amt;k>0;k--){
-//                        i+(amt-k)
-                        if (whichButton==5)
-                        {
+                    for (int k = amt; k > -1; k--) {
+                        if (whichButton == 5) {
+                            Button nextButton = pile_list_button.get(whichButton++);
+                            if (whosTurn)//player one's turn so allow  p1 mancala to go up
+                            {
+                                //extra k-- because one goes in mancala
+                                k--;
+                                int newValue = Integer.parseInt(p1_mancala_label.getText());
+                                newValue++;
+                                p1_mancala_label.setText(String.valueOf(newValue));
 
-                        }
-                        else if (whichButton==11){
+                                if (k > 0) {
+                                    newValue = Integer.parseInt(nextButton.getText());
+                                    newValue++;
+                                    nextButton.setText(String.valueOf(newValue));
+                                }
+                            } else {//else dont put anything into p1_mancala
+                                int newValue = Integer.parseInt(nextButton.getText());
+                                newValue++;
+                                nextButton.setText(String.valueOf(newValue));
+                            }
+                        } else if (whichButton == 11) {
+                            Button nextButton = pile_list_button.get(0);
+                            if (whosTurn)//if player one dont add to p2_mancala
+                            {
+                                int newValue = Integer.parseInt(nextButton.getText());
+                                newValue++;
+                                nextButton.setText(String.valueOf(newValue));
+                            } else {//add to p2 mancala cuz its p2's turn
+                                k--;
+                                int newValue = Integer.parseInt(p2_mancala_label.getText());
+                                newValue++;
+                                p2_mancala_label.setText(String.valueOf(newValue));
 
+                                if (k > 0) {
+                                    newValue = Integer.parseInt(nextButton.getText());
+                                    newValue++;
+                                    nextButton.setText(String.valueOf(newValue));
+                                }
+                            }
+                        } else {
+                            Button nextButton = pile_list_button.get(whichButton++);
+                            int newValue = Integer.parseInt(nextButton.getText());
+                            newValue++;
+                            nextButton.setText(String.valueOf(newValue));
                         }
-                        Button nextButton =  pile_list_button.get(whichButton);
+                        button.setText("0");
 
                     }
+                    whosTurn = !whosTurn;
+                    takeTurn();
 
-
-                    whosTurn= !whosTurn;
                 }
             });
         }
-        takeTurn();
     }
 
-    public void takeTurn(){
-        if (whosTurn){
+    public void takeTurn() {
+        if (whosTurn) {
             turn_label.setText("Player 1's Turn!");
-            for (int i =0; i <6; i++){
+            for (int i = 0; i < 6; i++) {
                 pile_list_button.get(i).setDisable(false);
             }
-            for (int i =6; i <12; i++){
+            for (int i = 6; i < 12; i++) {
                 pile_list_button.get(i).setDisable(true);
             }
-        }
-        else{
+        } else {
             turn_label.setText("Player 2's Turn!");
-            for (int i =0; i <6; i++){
+            for (int i = 0; i < 6; i++) {
                 pile_list_button.get(i).setDisable(true);
             }
-            for (int i =6; i <12; i++){
+            for (int i = 6; i < 12; i++) {
                 pile_list_button.get(i).setDisable(false);
             }
         }
     }
 
-    public void setSettings(){
+    public void setSettings() {
         player_settings.setText("Versus");
     }
 
